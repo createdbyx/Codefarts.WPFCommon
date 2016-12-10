1c356bee99b7dfec53598b9551b92c1e3a72671c
Dean Lunz <deanlunz@createdbyx.com>
2016-12-09 16:54:02 -0800

    added converter and set clipboard text command

---
+++ Commands/SetClipboardTextCommand.cs
@@ -0,0 +1,36 @@
+﻿namespace Codefarts.WPFCommon.Commands
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Input;
+
+    public class SetClipboardTextCommand : ICommand
+    {
+        public SetClipboardTextCommand(TextDataFormat dataFormat)
+        {
+            DataFormat = dataFormat;
+        }
+
+        public TextDataFormat DataFormat { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SetClipboardTextCommand"/> class.
+        /// </summary>
+        public SetClipboardTextCommand()
+        {
+            this.DataFormat = TextDataFormat.Text;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Clipboard.SetText(parameter.ToString(), this.DataFormat);
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
