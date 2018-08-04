d5d579372c3d1024950c8dee344d15c80b6dc220
Dean Lunz <deanlunz@createdbyx.com>
2018-08-04 15:30:00 -0700

    implements inotify propertyy changed interface

--- Commands/GenericDelegateCommand.cs
+++ Commands/GenericDelegateCommand.cs
@@ -1,20 +1,123 @@
 namespace Codefarts.WPFCommon.Commands
 {
     using System;
+    using System.ComponentModel;
+    using System.Linq.Expressions;
+    using System.Reflection;
     using System.Windows.Input;
+    using System.Xml.Serialization;
 
-    public class GenericDelegateCommand<T> : ICommand
+    public class GenericDelegateCommand<T> : INotifyPropertyChanged, ICommand
     {
         private Func<T, bool> canExecuteCallback;
         private Action<T> executeCallback;
 
+        private bool isNotifying;
+
         public event EventHandler Initialize;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="T:System.Object"/> class.
+        /// Initializes a new instance of the <see cref="GenericDelegateCommand"/> class.
         /// </summary>
         public GenericDelegateCommand()
         {
+            this.isNotifying = true;
+        }
+
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public virtual event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Enables/Disables property change notification.
+        /// Virtualized in order to help with document oriented view models.
+        /// </summary>
+        [XmlIgnore]
+        public virtual bool IsNotifying
+        {
+            get
+            {
+                return this.isNotifying;
+            }
+
+            set
+            {
+                var currentValue = this.isNotifying;
+                if (currentValue != value)
+                {
+                    this.isNotifying = value;
+                    this.NotifyOfPropertyChange(() => this.IsNotifying);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Raises a change notification indicating that all bindings should be refreshed.
+        /// </summary>
+        public virtual void Refresh()
+        {
+            this.NotifyOfPropertyChange(string.Empty);
+        }
+
+        /// <summary>
+        /// Notifies subscribers of the property change.
+        /// </summary>
+        /// <param name = "propertyName">Name of the property.</param>
+//#if NET || SILVERLIGHT || UNITY_5   
+        public virtual void NotifyOfPropertyChange(string propertyName)
+        {
+            //#else
+            //    public virtual void NotifyOfPropertyChange([System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+            //      {
+            //#endif
+            var handler = this.PropertyChanged;
+            if (this.IsNotifying && handler != null)
+            {
+                this.OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        /// <summary>
+        /// Notifies subscribers of the property change.
+        /// </summary>
+        /// <typeparam name = "TProperty">The type of the property.</typeparam>
+        /// <param name = "property">The property expression.</param>
+        public void NotifyOfPropertyChange<TProperty>(Expression<Func<TProperty>> property)
+        {
+            this.NotifyOfPropertyChange(this.GetMemberInfo(property).Name);
+        }
+
+        /// <summary>
+        /// Raises the <see cref="PropertyChanged" /> event directly.
+        /// </summary>
+        /// <param name="e">The <see cref="PropertyChangedEventArgs"/> instance containing the event data.</param>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        protected void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        private MemberInfo GetMemberInfo(Expression expression)
+        {
+            var lambda = (LambdaExpression)expression;
+
+            MemberExpression memberExpression;
+            if (lambda.Body is UnaryExpression)
+            {
+                var unaryExpression = (UnaryExpression)lambda.Body;
+                memberExpression = (MemberExpression)unaryExpression.Operand;
+            }
+            else
+            {
+                memberExpression = (MemberExpression)lambda.Body;
+            }
+
+            return memberExpression.Member;
         }
 
         /// <summary>
