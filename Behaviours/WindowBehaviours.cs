8e64a35b0b14e7697e1fa47912336354c4b65679
Dean Lunz <deanlunz@createdbyx.com>
2019-06-22 12:47:18 -0700

    updated window behaviours *Not tested

--- Behaviours/WindowBehaviours.cs
+++ Behaviours/WindowBehaviours.cs
@@ -1,5 +1,6 @@
 ﻿namespace Codefarts.WPFCommon.Behaviours
 {
+    using System;
     using System.Windows;
     using System.Windows.Input;
 
@@ -8,57 +9,144 @@
         #region Loaded
 
         public static readonly DependencyProperty LoadedCommandProperty =
-            DependencyProperty.RegisterAttached("LoadedCommand", typeof(ICommand), typeof(WindowBehaviours), new FrameworkPropertyMetadata(LoadedCommandChanged));
-
-        public static readonly DependencyProperty ClosingCommandProperty =
-            DependencyProperty.RegisterAttached("ClosingCommand", typeof(ICommand), typeof(WindowBehaviours), new FrameworkPropertyMetadata(ClosingCommandChanged));
+            DependencyProperty.RegisterAttached("Loaded", typeof(ICommand), typeof(WindowBehaviours), new FrameworkPropertyMetadata(LoadedCommandChanged));
 
         private static void LoadedCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             var element = (FrameworkElement)d;
-            element.Loaded += element_Loaded;
+            element.Loaded += Window_Loaded;
         }
+
+        private static void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var element = (FrameworkElement)sender;
+            var command = GetLoaded(element);
+            command.Execute(e);
+        }
+
+        public static void SetLoaded(UIElement element, ICommand value)
+        {
+            element.SetValue(LoadedCommandProperty, value);
+        }
+
+        public static ICommand GetLoaded(UIElement element)
+        {
+            return (ICommand)element.GetValue(LoadedCommandProperty);
+        }
+
+        #endregion
+
+        #region Closing
+
+        public static readonly DependencyProperty ClosingCommandProperty =
+          DependencyProperty.RegisterAttached("Closing", typeof(ICommand), typeof(WindowBehaviours), new FrameworkPropertyMetadata(ClosingCommandChanged));
 
         private static void ClosingCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            var element = (Window)d;
-            element.Closing += Window_Closing;
+            if (d is Window window)
+            {
+                if (e.NewValue != null)
+                {
+                    window.Closing += Window_Closing;
+                }
+                else
+                {
+                    window.Closing -= Window_Closing;
+                }
+            }
         }
 
         private static void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             var element = (Window)sender;
-            var command = GetClosingCommand(element);
-            command.Execute(e);
+            var closing = GetClosing(element);
+            if (closing != null)
+            {
+                if (closing.CanExecute(e))
+                {
+                    closing.Execute(e);
+                }
+                else
+                {
+                    var cancelClosing = GetCancelClosing(element);
+                    if (cancelClosing != null)
+                    {
+                        if (cancelClosing.CanExecute(e))
+                        {
+                            cancelClosing.Execute(e);
+                        }
+                    }
+                }
+            }
         }
 
-        static void element_Loaded(object sender, RoutedEventArgs e)
-        {
-            var element = (FrameworkElement)sender;
-            var command = GetLoadedCommand(element);
-            command.Execute(e);
-        }
-
-        public static void SetLoadedCommand(UIElement element, ICommand value)
-        {
-            element.SetValue(LoadedCommandProperty, value);
-        }
-
-        public static void SetClosingCommand(Window element, ICommand value)
+        public static void SetClosing(Window element, ICommand value)
         {
             element.SetValue(ClosingCommandProperty, value);
         }
 
-        public static ICommand GetLoadedCommand(UIElement element)
-        {
-            return (ICommand)element.GetValue(LoadedCommandProperty);
-        }
-
-        public static ICommand GetClosingCommand(Window window)
+        public static ICommand GetClosing(Window window)
         {
             return (ICommand)window.GetValue(ClosingCommandProperty);
         }
 
+        #endregion
+
+        #region CancelClosing
+
+        public static ICommand GetCancelClosing(DependencyObject obj)
+        {
+            return (ICommand)obj.GetValue(CancelClosingProperty);
+        }
+
+        public static void SetCancelClosing(DependencyObject obj, ICommand value)
+        {
+            obj.SetValue(CancelClosingProperty, value);
+        }
+
+        public static readonly DependencyProperty CancelClosingProperty =
+            DependencyProperty.RegisterAttached("CancelClosing", typeof(ICommand), typeof(WindowBehaviours));
+
+        #endregion
+
+        #region Closed
+        public static ICommand GetClosed(DependencyObject obj)
+        {
+            return (ICommand)obj.GetValue(ClosedProperty);
+        }
+
+        public static void SetClosed(DependencyObject obj, ICommand value)
+        {
+            obj.SetValue(ClosedProperty, value);
+        }
+
+        public static readonly DependencyProperty ClosedProperty = DependencyProperty.RegisterAttached("Closed", typeof(ICommand), typeof(WindowBehaviours),
+            new UIPropertyMetadata(ClosedChanged));
+
+        private static void ClosedChanged(DependencyObject target, DependencyPropertyChangedEventArgs e)
+        {
+            var window = target as Window;
+            if (window != null)
+            {
+                if (e.NewValue != null)
+                {
+                    window.Closed += Window_Closed;
+                }
+                else
+                {
+                    window.Closed -= Window_Closed;
+                }
+            }
+        }
+
+        static void Window_Closed(object sender, EventArgs e)
+        {
+            var closed = GetClosed(sender as Window);
+            if (closed != null)
+            {
+                closed.Execute(null);
+            }
+        }
         #endregion
     }
 }
